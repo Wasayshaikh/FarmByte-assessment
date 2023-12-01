a4955d330c8f7c030fd81e86a824f488b97f2fe6
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2023-12-01 21:10:42 +0500

    add the api for getting the post

--- src/views/posts/Posts.tsx
+++ src/views/posts/Posts.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+
 
 export default function Posts() {
+  const [post, setPost] = useState<null | Array<object>>(null);
+  useEffect(() => {
+     const getPost = async ():Promise<void> => {
+      try {
+        const posts = await fetch("https://jsonplaceholder.typicode.com/posts", {
+
+          method: "GET"
+        }
+
+        ).then(r => r.json())
+        setPost(posts)
+      } catch (error) {
+        
+      }
+
+    }
+    getPost()
+
+
+  }, [])
+  console.log(typeof(post))
+  console.log(post)
   return (
-    <div>Posts</div>
+   
+    <div>
+      {post === null ? (<p>loading</p>):post.map((pp:any)=>(
+        <div key={pp.id}>{pp.title}</div>
+      ))
+
+      }
+    </div>
   )
 }
