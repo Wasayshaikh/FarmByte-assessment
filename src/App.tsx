771f4f984f7a2b49024eaa22ea4d663509f3f1b1
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2023-12-01 18:01:50 +0500

    setting routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 
 import Navbar from './components/Navbar'
-
+import { Routes, Route,Navigate  } from "react-router-dom";
+import { Home, Posts } from './views';
 function App() {
 
 
   return (
-    <>
+    <div>
      <Navbar />
-    </>
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/post" element={<Navigate to='/posts' />} />
+      </Routes>
+    </div>
   )
 }
 
