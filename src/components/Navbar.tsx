1eca75df1fcdd842979b6fbd1f954a7c2f718a41
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2023-12-01 21:10:08 +0500

    add links to the nav item and fix dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,47 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon as FAIcons } from "@fortawesome/react-fontawesome";
 import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
+import { Link } from 'react-router-dom';
 
 export default function Navbar(): JSX.Element {
-    const [openNav, setOpenNav] = React.useState<boolean>(false);
-    const [mobileScreen, setMobileScreen] = React.useState<boolean>(false);
+    const [openNav, setOpenNav] = useState<boolean>(false);
+    const [mobileScreen, setMobileScreen] = useState<boolean>(false);
 
-    React.useEffect(() => {
+    useEffect(() => {
         const handleResize = () => {
-            setMobileScreen(window.innerWidth < 960);
+            setMobileScreen(window.innerWidth < 768);
+            if (window.innerWidth > 768) {
+                setOpenNav(false);
+            }
         };
 
         // Initial setup
         handleResize();
 
         window.addEventListener('resize', handleResize);
-
+        window.addEventListener('click', () => { setOpenNav(false); })
         // Cleanup on component unmount
         return () => {
             window.removeEventListener('resize', handleResize);
         };
     }, [mobileScreen]);
 
-    const openMobileNav = () => {
+    const openMobileNav = (event: React.MouseEvent<SVGSVGElement>) => {
+        event.stopPropagation();
         setOpenNav(!openNav);
     }
     return (
-        <nav>
+        <nav className='lg:container m-auto'>
             <div className='w-100  flex justify-between p-2'>
                 <div className='p-2'>
                     <span className='text-lg'>Nav Heading</span>
                 </div>
                 <ul className={` ${mobileScreen ? `hidden` : `flex`}`}>
-                    <li className='p-2'><a href="#">Home</a></li>
-                    <li className='p-2'><a href="#">Post</a></li>
+                    <li className='p-2'><Link to={'/'}> Home </Link></li>
+                    <li className='p-2'><Link to={'/posts'}> Post </Link></li>
                 </ul>
                 <div className={`p-2   ${mobileScreen ? `` : `hidden`}`}>
                     <FAIcons className=' cursor-pointer' icon={faBars} size={'xl'} onClick={openMobileNav} />
                 </div>
             </div>
-            <ul className={`px-4 bg-green-300 h-auto max-h-0 ${openNav ? `!max-h-[200px]` : ``} overflow-hidden transition-all duration-150 delay-[display, 150s]`}>
-                <li className='p-2 cursor-pointer'>asd</li>
-                <li className='p-2 cursor-pointer'>asd</li>
+            <ul className={`px-4 bg-green-300 h-auto max-h-0 absolute right-3 w-52 ${openNav ? `!max-h-[200px]` : ``} md:hidden overflow-hidden transition-all duration-150 delay-[display, 150s]`}>
+                <li className='p-2 cursor-pointer'><Link to={'/'}> Home </Link></li>
+                <li className='p-2 cursor-pointer'><Link to={'/posts'}> Post </Link></li>
             </ul>
         </nav>
     )
