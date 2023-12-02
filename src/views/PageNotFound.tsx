2a7b50a22cf3c0a02de44a96b7992a534179b4e3
abdul wasay <abdulwasayshaikh1999@gmail.com>
2023-12-02 22:02:05 +0500

    created the 404 view component

---
+++ src/views/PageNotFound.tsx
@@ -0,0 +1,9 @@
+
+const PageNotFound = () => {
+    return (
+      <div className=" absolute top-2/4  text-center w-full p-3">
+        <h1 className=" text-3xl font-bold">404 Page Not Found</h1>
+      </div>
+    )
+  }
+  export default  PageNotFound
