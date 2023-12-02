b87800fe2d05575b17588cd68e1ab09633f6cc16
abdul wasay <abdulwasayshaikh1999@gmail.com>
2023-12-02 22:01:45 +0500

    add 404 component

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,3 +1,4 @@
 export { default as Home} from "./Home";
 export { default as Posts} from "./posts/Posts";
 export { default as SinglePost } from "./posts/SinglePost"
+export { default as PageNotFound } from "./PageNotFound"
