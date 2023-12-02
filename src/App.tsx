f767c61564c6c6563463e793d85482b81971da40
abdul wasay <abdulwasayshaikh1999@gmail.com>
2023-12-02 21:25:48 +0500

    Add single post route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 
 import Navbar from './components/Navbar'
 import { Routes, Route,Navigate  } from "react-router-dom";
-import { Home, Posts } from './views';
+import { Home, SinglePost, Posts } from './views';
 function App() {
 
 
   return (
-    <div>
+    <div className=' bg-gray-300'>
      <Navbar />
      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/posts" element={<Posts />} />
         <Route path="/post" element={<Navigate to='/posts' />} />
+        <Route path="/posts/:id" element={<SinglePost />} />
       </Routes>
     </div>
   )
