48c4f48578823fb970a43c45bf263832c5fe8d28
abdul wasay <abdulwasayshaikh1999@gmail.com>
2023-12-02 21:26:59 +0500

    welcome page set

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
 
-export default function Home() {
+const Home = () => {
   return (
-    <div>Home</div>
+    <div className=" absolute top-2/4  text-center w-full p-3">
+      <h1 className=" text-3xl font-bold">FarmByte Assessment Project</h1>
+    </div>
   )
 }
+export default  Home
